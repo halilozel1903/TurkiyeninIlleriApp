1ee7b768ff711d5f5ade8e6540a2fa5139bceace
Halil Özel <halilozel1903@gmail.com>
2018-10-15 18:51:36 +0300

    new project

---
+++ TurkiyeninIlleri/RoundButton.swift
@@ -0,0 +1,41 @@
+//
+//  RoundButton.swift
+//  TurkiyeninIlleri
+//
+//  Created by Halil Özel on 15.10.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class RoundButton : UIButton{
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+    }
+
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+}
---
+++ TurkiyeninIlleri/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TurkiyeninIlleri
+//
+//  Created by Halil Özel on 15.10.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
